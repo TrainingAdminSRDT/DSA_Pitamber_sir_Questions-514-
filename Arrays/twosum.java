7586d6b29abeb7b3b8e63cafb1415d12c5b93adf
Kritika Kapoor <kritikakapoor131@gmail.com>
2026-06-09 12:22:56 +0530

    Add Arrays/twosum.java

---
+++ Arrays/twosum.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class twosum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> set = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int c = target - nums[i];
+
+            if (set.containsKey(c)) {
+                return new int[]{set.get(c), i};
+            }
+
+            set.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println("Indices: " + Arrays.toString(result));
+    }
+}
