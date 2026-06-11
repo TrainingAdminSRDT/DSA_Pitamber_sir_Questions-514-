89515165e7bc9f6b9b8535899bc77216290c3327
Shivansh Rathor <shivanshrathore61@gmail.com>
2026-06-12 01:41:02 +0530

    Implement firstUniqChar method to find the first unique character in a string

--- First.java
+++ First.java
@@ -1,5 +1,30 @@
-package DSA_Pitamber_sir_Questions-514-;
+import java.util.HashMap;
 
 public class First {
+
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> table = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(table.containsKey(s.charAt(i))){
+                int value=table.get(s.charAt(i));
+                table.put(s.charAt(i),value++);
+            }else{
+                table.put(s.charAt(i),1);
+            }
+        }
+        for(int i=0;i<s.length();i++){
+            int value=table.get(s.charAt(i));
+            if(value==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        First f=new First();
+        String s="leetcode";
+        System.out.println(f.firstUniqChar(s));
+    }
     
 }
