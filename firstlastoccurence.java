dc41cc243e9a5484f131e230552ff65bfc99794c
Ayush Pandey <ayushpandey2429@gmail.com>
2026-06-13 10:14:13 +0530

    Add First last Occurence Solution

---
+++ firstlastoccurence.java
@@ -0,0 +1,55 @@
+ import java.util.*;
+
+public class FirstLastOccurrence {
+
+    static int firstOccurrence(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                high = mid - 1; // search left part
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    static int lastOccurrence(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                low = mid + 1; // search right part
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 2, 3, 4, 5};
+        int target = 2;
+
+        int first = firstOccurrence(arr, target);
+        int last = lastOccurrence(arr, target);
+
+        System.out.println("First Occurrence: " + first);
+        System.out.println("Last Occurrence: " + last);
+    }
+}
